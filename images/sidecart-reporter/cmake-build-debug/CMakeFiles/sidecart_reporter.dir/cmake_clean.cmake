c155d2f436a53a7e44b917b8b3ff87fb95740060
tin <ludovicic@unipu.hr>
2021-04-07 10:15:45 +0200

    prometh-sidecar-cnt

---
+++ images/sidecart-reporter/cmake-build-debug/CMakeFiles/sidecart_reporter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sidecart_reporter.dir/main.c.o"
+  "sidecart_reporter"
+  "sidecart_reporter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sidecart_reporter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ images/sidecart-reporter/server.py
@@ -0,0 +1,38 @@
+import os
+import psutil
+
+print(os.getpid())
+
+c=0
+for process in psutil.process_iter ():
+    c=c+1
+    Name = process.name () # Name of the process
+    ID = process.pid # ID of the process
+    print ("Process name =", Name ,",","Process ID =", ID)
+print ("\nTotal number of running process are ", c)
+
+import subprocess
+test = subprocess.Popen(["ping","-W","2","-c", "1", "192.168.0.1"], stdout=subprocess.PIPE)
+output = test.communicate()[0]
+
+print(output)
+
+
+import subprocess
+
+s = subprocess.check_output(["echo", "Hello World!"])
+
+c=0
+for process in psutil.process_iter ():
+    c=c+1
+    Name = process.name () # Name of the process
+    ID = process.pid # ID of the process
+    print ("Process name =", Name ,",","Process ID =", ID)
+print ("\nTotal number of running process are ", c)
+print(str(s))
+
+import dpkt, pcap
+pc = pcap.pcap()     # construct pcap object
+pc.setfilter('icmp') # filter out unwanted packets
+for timestamp, packet in pc:
+    print(dpkt.ethernet.Ethernet(packet))
